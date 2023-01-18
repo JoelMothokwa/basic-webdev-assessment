3994ffc5d2a4c60210a85dfbdee7be2efae0e413
JoelMothokwa <122977166+JoelMothokwa@users.noreply.github.com>
2023-01-18 12:50:29 +0200

    Update todo.class.php

--- api/todo.class.php
+++ api/todo.class.php
@@ -6,5 +6,43 @@ class Todo {
         public string $title, 
         public string $description = '',
         public bool $done = false
-    ) {}
+    ) {
+        
+        $this->id = $id;
+        $this->title = $title;
+        $this->description = $description;
+        $this->done = $done;
+    }
+
+    public function setID(string $id = null) {
+        $this->id = $id;
+    }
+
+    public function getID(): string {
+        return $this->id;
+    }
+
+    public function setTitle(string $title = null) {
+        $this->title = $title;
+    }
+
+    public function getTitle(): string {
+        $this->title;
+    }
+
+    public function setDescription(string $description = '') {
+        $this->description = $description;
+    }
+
+    public function getDescription(): string {
+        return $this->description;
+    }
+
+    public function setDone(bool $done = false) {
+        $this->done = $done;
+    }
+
+    public function getDone(): bool {
+        return $this->done;
+    }
 }
