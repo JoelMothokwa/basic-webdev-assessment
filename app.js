7322790c694d01563a95415a494f5f9a7596dc0c
JoelMothokwa <122977166+JoelMothokwa@users.noreply.github.com>
2023-01-18 12:53:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,52 @@
 function putTodo(todo) {
     // implement your code here
     console.log("calling putTodo");
+    fetch(window.location.href + 'api/todo', {
+        method: "PUT",
+        headers: {"Content-Type": "application/json;charset=UTF-8"},
+        body: JSON.stringify(todo)
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(
+    //error => showToastMessage('Failed to create todo...');
+    error=>console.log(error)
+    );
     console.log(todo);
 }
 
 function postTodo(todo) {
     // implement your code here
     console.log("calling postTodo");
-    console.log(todo);
+    fetch(window.location.href + 'api/todo', {
+        method: "POST",
+        headers: {"Content-Type": "application/json;charset=UTF-8"},
+        body: JSON.stringify(todo)
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(
+    //error => showToastMessage('Failed to create todo...');
+    error=>console.log(error)
+    );
+    
 }
 
 function deleteTodo(todo) {
     // implement your code here
     console.log("calling deleteTodo");
-    console.log(todo);
+    fetch(window.location.href + 'api/todo', {
+        method: "DELETE",
+        headers: {"Content-Type": "application/json;charset=UTF-8"},
+        body: JSON.stringify(todo)
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(
+    //error => showToastMessage('Failed to create todo...');
+    error=>console.log(error)
+    );
+    //console.log(todo);
 }
 
 // example using the FETCH API to do a GET request
